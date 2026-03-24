d3518b35cb6f91717a288990f250125915047aad
richardzatko <ar.zatko@gmail.com>
2026-03-24 14:27:35 +0000

    Create campaign-members-log.js

---
+++ api/campaign-members-log.js
@@ -0,0 +1,19 @@
+import { readLogs } from './_log-store.js';
+
+export default async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    return res.status(200).end();
+  }
+
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  const logs = readLogs();
+  return res.status(200).json({ runs: logs });
+}
